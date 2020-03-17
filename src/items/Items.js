d04d8a79aefb7c0ebf4f3fcff9de470b172a70dd
rakibtg <rakibtg@gmail.com>
2020-03-17 11:43:23 +0600

    use value from props

--- src/items/Items.js
+++ src/items/Items.js
@@ -2,15 +2,13 @@ import React from 'react'
 
 const Items = ({ items = [] }) => {
 
-    const [listOfItems, itemsUpdator] = React.useState(items)
-
-    if (listOfItems.length <= 0) {
+    if (items.length <= 0) {
         return <div>No items found</div>
     } else {
         return <div>
             <ol>
                 {
-                    listOfItems.map((item, index) => {
+                    items.map((item, index) => {
                         return <li key={index}>{item}</li>
                     })
                 }
